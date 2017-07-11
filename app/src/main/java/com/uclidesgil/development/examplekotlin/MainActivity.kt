554e98be079b160f2d209c2e4038ed0fc0ff68bb
Uclides Gil <uclidesgil@gmail.com>
2017-07-11 18:28:20 -0400

    ending example

--- app/src/main/java/com/uclidesgil/development/examplekotlin/MainActivity.kt
+++ app/src/main/java/com/uclidesgil/development/examplekotlin/MainActivity.kt
@@ -9,13 +9,15 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.txtTimer
 import kotlinx.android.synthetic.main.activity_main.txttimerElapsed
 import kotlinx.android.synthetic.main.activity_main.button
-import com.uclidesgil.development.examplekotlin.MainActivity.MalibuCountDownTimer
+
+
+
 
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val countDownTimer: MalibuCountDownTimer? = null
+    private var countDownTimer: MalibuCountDownTimer? = null
     private var timeElapsed: Long = 0
     private var timerHasStarted: Boolean = false
 
@@ -27,18 +29,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        countDownTimer = MalibuCountDownTimer(startTime, interval)
+        button.setOnClickListener(this)
 
+        txtTimer.setText(" " + txtTimer.getText() +" "+ startTime.toString())
     }
 
-    override fun onClick(p0: View?) {
+    override fun onClick(p0: View) {
      when(p0){
          button -> {
-             timerHasStarted = true
-             button?.setText("start")
-         }
-         else -> {
-             timerHasStarted = false
-             button?.setText("reset")
+             if(!timerHasStarted){
+                 countDownTimer?.start()
+                 timerHasStarted = true
+                 button.setText("start")
+             }
+             else {
+                 countDownTimer?.cancel()
+                 timerHasStarted = false
+                 button.setText("reset")
+             }
          }
      }
     }
